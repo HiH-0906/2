faba9e506b749ba9109b9ddfce8b4e2703a52c17
HiH-0906 <homuihomu0906@gmail.com>
2020-07-08 17:55:43 +0900

    1

---
+++ shootemup/shootemup/Spiral.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <DxLib.h>
+#include"Geometry.h"
+#include "bullet.h"
+#include "BulMng.h"
+
+struct Spiral
+{
+	static int angle;
+	void operator()(Position2 enemy, Position2 player, std::vector<shardBul>& list)
+	{
+		auto vec = Vector2(cosf((angle) * (DX_PI_F / 180.0f)), sinf((angle) * (DX_PI_F / 180.0f)));
+		list.emplace_back(new bullet(enemy, vec * 2));
+		list.emplace_back(new bullet(enemy, vec * -2));
+		angle += 10;
+	}
+};
+int Spiral::angle = 0;
