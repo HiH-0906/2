2fda5eb75614dbe302974c0162d2470bb5dda47e
HiH-0906 <homuihomu0906@gmail.com>
2020-07-31 13:54:48 +0900

    a

---
+++ Project2/OzyamaPuyo.cpp
@@ -0,0 +1,44 @@
+#include "OzyamaPuyo.h"
+#include "SceneMng.h"
+
+OzyamaPuyo::OzyamaPuyo()
+{
+}
+
+OzyamaPuyo::OzyamaPuyo(Vector2&& pos, PUYO_ID id)
+{
+	playPuyo_ = false;
+	pos_ = pos;
+	rad_ = PUYO_RAD;
+	activ_ = true;
+	id_ = PUYO_ID::OZAYMA;
+	puyonCnt_ = 0;
+	puyonMax_ = 12;
+	muyonCnt_ = 0;
+	muyonMax_ = 12;
+	downCnt_ = 0;
+	downNum_ = 15;
+	dirpermit_.per = 0;
+	oldDirpermit_.per = 0;
+	munyonmit_.per = 0;
+	vec_ = {
+			{INPUT_ID::LEFT,Vector2{-rad_ * 2,0}},
+			{INPUT_ID::RIGHT,Vector2{rad_ * 2,0}},
+			{INPUT_ID::UP,Vector2{0,-rad_ * 2}},
+			{INPUT_ID::DOWN,Vector2{0,rad_ / 2}}
+	};
+}
+
+OzyamaPuyo::~OzyamaPuyo()
+{
+}
+
+void OzyamaPuyo::SetMuyonCnt(void)
+{
+}
+
+bool OzyamaPuyo::SetPuyonCnt(void)
+{
+	// ‰½‚à‚µ‚È‚¢
+	return false;
+}
---
+++ Project2/OzyamaPuyo.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "common/Vector2.h"
+#include "Puyo.h"
+
+class OzyamaPuyo :
+	public Puyo
+{
+public:
+	OzyamaPuyo();
+	OzyamaPuyo(Vector2&& pos, PUYO_ID id);
+	~OzyamaPuyo();
+	void SetMuyonCnt(void)override;
+	bool SetPuyonCnt(void)override;
+private:
+};
+
