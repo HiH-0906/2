4d930e4ee75ad9a2740449aa2b467dcd49de840e
HIH-0906 <homuihomu0906@gmail.com>
2020-10-13 13:38:14 +0900

    例トレー

---
+++ RayTracing/RayTracing/RayTracing/main.cpp
@@ -0,0 +1,50 @@
+#include<dxlib.h>
+#include"Geometry.h"
+const int screen_width = 640;
+const int screen_height = 480;
+
+//ヒントになると思って、色々と関数を用意しておりますが
+//別にこの関数を使わなければいけないわけでも、これに沿わなければいけないわけでも
+//ありません。レイトレーシングができていれば構いません。
+
+///レイ(光線)と球体の当たり判定
+///@param ray (視点からスクリーンピクセルへのベクトル)
+///@param sphere 球
+///@hint レイは正規化しといたほうが使いやすいだろう
+bool IsHitRayAndObject(const Position3& eye,const Vector3& ray,const Sphere& sp) {
+	//レイが正規化済みである前提で…
+	//
+	//視点から球体中心へのベクトル(視線)を作ります
+	//
+	//中心から視線への内積をとります＝＞ベクトル長
+	//視線ベクトルとベクトル長をかけて、中心からの垂線下した点を求めます
+
+	return false;
+}
+
+///レイトレーシング
+///@param eye 視点座標
+///@param sphere 球オブジェクト(そのうち複数にする)
+void RayTracing(const Position3& eye,const Sphere& sphere) {
+	for (int y = 0; y < screen_height; ++y) {//スクリーン縦方向
+		for (int x = 0; x < screen_width; ++x) {//スクリーン横方向
+			//①視点とスクリーン座標から視線ベクトルを作る
+			//②正規化しとく
+			//③IsHitRay関数がTrueだったら白く塗りつぶす
+			//※塗りつぶしはDrawPixelという関数を使う。
+			DrawPixel(x, y, 0x007700);
+		}
+	}
+}
+
+int WINAPI WinMain(HINSTANCE , HINSTANCE,LPSTR,int ) {
+	ChangeWindowMode(true);
+	SetGraphMode(screen_width, screen_height, 32);
+	SetMainWindowText(_T("簡易版のレイトレでっせ"));
+	DxLib_Init();
+
+	RayTracing(Vector3(0, 0, 300), Sphere(100, Position3(0, 0, -100)));
+
+	WaitKey();
+	DxLib_End();
+}
