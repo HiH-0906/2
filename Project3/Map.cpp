71ea4e0cb9e0c4d9c71282374f5519b235552f49
HiH-0906 <homuihomu0906@gmail.com>
2020-10-20 18:35:26 +0900

    map

---
+++ Project3/Map.cpp
@@ -0,0 +1,14 @@
+#include "Map.h"
+
+Map::Map()
+{
+}
+
+Map::~Map()
+{
+}
+
+bool Map::LoadMap(void)
+{
+	return false;
+}
---
+++ Project3/Map.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <map>
+#include <string>
+#include <vector>
+
+class Map
+{
+public:
+	Map();
+	~Map();
+
+	bool LoadMap(void);
+private:
+};
+
