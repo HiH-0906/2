66e9e57424cf4d8ab5e6bce668eb26c4be53d622
HIH-0906 <homuihomu0906@gmail.com>
2020-10-20 15:21:18 +0900

    レイトレ円表示 丹次郎

--- RayTracing/RayTracing/RayTracing/main.cpp
+++ RayTracing/RayTracing/RayTracing/main.cpp
@@ -14,11 +14,18 @@ const int screen_height = 480;
 bool IsHitRayAndObject(const Position3& eye,const Vector3& ray,const Sphere& sp) {
 	//レイが正規化済みである前提で…
 	//
-	//視点から球体中心へのベクトル(視線)を作ります
+	//視点から球体中心へのベクトルを作ります
 	//
-	//中心から視線への内積をとります＝＞ベクトル長
+	auto c = sp.pos - eye;
+	//中心から視線への内積をとります＝＞射影長（ベクトル長として利用）
+	auto v = Dot(c, ray);
 	//視線ベクトルとベクトル長をかけて、中心からの垂線下した点を求めます
-
+	auto r = ray * v;
+	r = c - r;
+	if (r.Magnitude() <= sp.radius)
+	{
+		return true;
+	}
 	return false;
 }
 
@@ -29,10 +36,22 @@ void RayTracing(const Position3& eye,const Sphere& sphere) {
 	for (int y = 0; y < screen_height; ++y) {//スクリーン縦方向
 		for (int x = 0; x < screen_width; ++x) {//スクリーン横方向
 			//①視点とスクリーン座標から視線ベクトルを作る
+			auto ray = Position3(x - screen_width / 2, -y + screen_height / 2, 0) - eye;
 			//②正規化しとく
-			//③IsHitRay関数がTrueだったら白く塗りつぶす
-			//※塗りつぶしはDrawPixelという関数を使う。
-			DrawPixel(x, y, 0x007700);
+			ray.Normalize();
+			//③IsHitRayAndObject関数がTrueだったら白く塗りつぶす
+			if (IsHitRayAndObject(eye, ray, sphere))
+			{
+				//※塗りつぶしはDrawPixelという関数を使う。
+				DrawPixel(x, y, 0xff0000);
+			}
+			else
+			{
+				if ((x / 40 + y / 40) % 2 == 0)
+				{
+					DrawPixel(x, y, 0x007700);
+				}
+			}
 		}
 	}
 }
@@ -43,7 +62,9 @@ int WINAPI WinMain(HINSTANCE , HINSTANCE,LPSTR,int ) {
 	SetMainWindowText(_T("簡易版のレイトレでっせ"));
 	DxLib_Init();
 
-	RayTracing(Vector3(0, 0, 300), Sphere(100, Position3(0, 0, -100)));
+	auto eye = Vector3(0, 0, 300);
+	auto sphere = Sphere(100, Position3(0, 0, -100));
+	RayTracing(eye, sphere);
 
 	WaitKey();
 	DxLib_End();
